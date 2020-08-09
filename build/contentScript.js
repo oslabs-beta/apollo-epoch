41d86295c8df081134320d37850b8e9f42df2c3e
Matt Severyn <mseveryn@gmail.com>
2020-08-09 00:28:51 -0700

    Webpack Finished for test mode

---
+++ build/contentScript.js
@@ -0,0 +1,309 @@
+/* -------------------------------------------------- */
+
+/*  Start of Webpack Chrome Hot Extension Middleware  */
+
+/* ================================================== */
+
+/*  This will be converted into a lodash templ., any  */
+
+/*  external argument must be provided using it       */
+
+/* -------------------------------------------------- */
+(function (chrome, window) {
+  var signals = JSON.parse('{"SIGN_CHANGE":"SIGN_CHANGE","SIGN_RELOAD":"SIGN_RELOAD","SIGN_RELOADED":"SIGN_RELOADED","SIGN_LOG":"SIGN_LOG","SIGN_CONNECT":"SIGN_CONNECT"}');
+  var config = JSON.parse('{"RECONNECT_INTERVAL":2000,"SOCKET_ERR_CODE_REF":"https://tools.ietf.org/html/rfc6455#section-7.4.1"}');
+  var reloadPage = "true" === "true";
+  var wsHost = "ws://localhost:9090";
+  var SIGN_CHANGE = signals.SIGN_CHANGE,
+      SIGN_RELOAD = signals.SIGN_RELOAD,
+      SIGN_RELOADED = signals.SIGN_RELOADED,
+      SIGN_LOG = signals.SIGN_LOG,
+      SIGN_CONNECT = signals.SIGN_CONNECT;
+  var RECONNECT_INTERVAL = config.RECONNECT_INTERVAL,
+      SOCKET_ERR_CODE_REF = config.SOCKET_ERR_CODE_REF;
+  var runtime = chrome.runtime,
+      tabs = chrome.tabs;
+  var manifest = runtime.getManifest(); // =============================== Helper functions ======================================= //
+
+  var formatter = function formatter(msg) {
+    return "[ WCER: ".concat(msg, " ]");
+  };
+
+  var logger = function logger(msg) {
+    var level = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : "info";
+    return console[level](formatter(msg));
+  };
+
+  var timeFormatter = function timeFormatter(date) {
+    return date.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
+  }; // ========================== Called only on content scripts ============================== //
+
+
+  function contentScriptWorker() {
+    runtime.sendMessage({
+      type: SIGN_CONNECT
+    }, function (msg) {
+      return console.info(msg);
+    });
+    runtime.onMessage.addListener(function (_ref) {
+      var type = _ref.type,
+          payload = _ref.payload;
+
+      switch (type) {
+        case SIGN_RELOAD:
+          logger("Detected Changes. Reloading ...");
+          reloadPage && window.location.reload();
+          break;
+
+        case SIGN_LOG:
+          console.info(payload);
+          break;
+      }
+    });
+  } // ======================== Called only on background scripts ============================= //
+
+
+  function backgroundWorker(socket) {
+    runtime.onMessage.addListener(function (action, sender, sendResponse) {
+      if (action.type === SIGN_CONNECT) {
+        sendResponse(formatter("Connected to Chrome Extension Hot Reloader"));
+      }
+    });
+    socket.addEventListener("message", function (_ref2) {
+      var data = _ref2.data;
+
+      var _JSON$parse = JSON.parse(data),
+          type = _JSON$parse.type,
+          payload = _JSON$parse.payload;
+
+      if (type === SIGN_CHANGE) {
+        tabs.query({
+          status: "complete"
+        }, function (loadedTabs) {
+          loadedTabs.forEach(function (tab) {
+            return tab.id && tabs.sendMessage(tab.id, {
+              type: SIGN_RELOAD
+            });
+          });
+          socket.send(JSON.stringify({
+            type: SIGN_RELOADED,
+            payload: formatter("".concat(timeFormatter(new Date()), " - ").concat(manifest.name, " successfully reloaded"))
+          }));
+          runtime.reload();
+        });
+      } else {
+        runtime.sendMessage({
+          type: type,
+          payload: payload
+        });
+      }
+    });
+    socket.addEventListener("close", function (_ref3) {
+      var code = _ref3.code;
+      logger("Socket connection closed. Code ".concat(code, ". See more in ").concat(SOCKET_ERR_CODE_REF), "warn");
+      var intId = setInterval(function () {
+        logger("Attempting to reconnect (tip: Check if Webpack is running)");
+        var ws = new WebSocket(wsHost);
+        ws.addEventListener("open", function () {
+          clearInterval(intId);
+          logger("Reconnected. Reloading plugin");
+          runtime.reload();
+        });
+      }, RECONNECT_INTERVAL);
+    });
+  } // ======================= Bootstraps the middleware =========================== //
+
+
+  runtime.reload ? backgroundWorker(new WebSocket(wsHost)) : contentScriptWorker();
+})(chrome, window);
+/* ----------------------------------------------- */
+
+/* End of Webpack Chrome Hot Extension Middleware  */
+
+/* ----------------------------------------------- *//******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./testSetup/chromeExtension/contentScript.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./testSetup/chromeExtension/contentScript.js":
+/*!****************************************************!*\
+  !*** ./testSetup/chromeExtension/contentScript.js ***!
+  \****************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
+  switch (msg.msg) {
+    case 'getDOM':
+      sendResponse({
+        msg: document.getElementsByTagName('body')[0].innerHTML
+      });
+      break;
+
+    case 'rerenderDOM':
+      console.log(msg);
+      var tab;
+
+      if (document.getElementsByClassName('snapShot').length === 0) {
+        tab = document.createElement('div');
+      } else {
+        tab = document.getElementsByClassName('snapShot')[0];
+      }
+
+      var p = document.createElement('p');
+      p.addEventListener('click', function () {
+        document.getElementsByTagName('body')[0].removeChild(tab);
+      });
+      p.innerText = 'Close Snapshot';
+      p.setAttribute('style', 'position:fixed; top: 10; left: 10');
+      tab.className = 'snapShot';
+      tab.setAttribute('style', 'position: fixed; background-color: white; width: 100%; height: 100%; top: 0; left: 0; border: 1px solid blue;');
+      tab.innerHTML = msg.newBody;
+      tab.prepend(p);
+      document.getElementsByTagName('body')[0].appendChild(tab);
+      sendResponse({
+        msg: 'rerender done'
+      });
+      break;
+
+    case 'getCache':
+      console.log('this is getCache message', msg);
+      runInPageContext(function () {
+        window.postMessage({
+          type: 'FROM_PAGE',
+          text: window.__APOLLO_CLIENT__.localState.cache.data.data
+        }, '*');
+      }, 'xx-XX');
+      sendResponse({
+        msg: 'cache requested'
+      });
+      break;
+
+    case 'retrieveCache':
+      sendResponse({
+        msg: cache
+      });
+      break;
+  }
+}); // var port = chrome.runtime.connect();
+
+var cache;
+window.addEventListener('message', function (event) {
+  // We only accept messages from ourselves
+  if (event.source != window) return;
+
+  if (event.data.type && event.data.type == 'FROM_PAGE') {
+    console.log("Content script received: ".concat(event.data.text));
+    cache = event.data.text;
+  }
+}, false); // Breaks out of the content script context by injecting a specially
+// constructed script tag and injecting it into the page.
+
+var runInPageContext = function runInPageContext(method) {
+  // The stringified method which will be parsed as a function object.
+  var stringifiedMethod = method instanceof Function ? method.toString() : "() => { ".concat(method, " }"); // The stringified arguments for the method as JS code that will reconstruct the array.
+
+  for (var _len = arguments.length, args = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+    args[_key - 1] = arguments[_key];
+  }
+
+  var stringifiedArgs = JSON.stringify(args); // The full content of the script tag.
+
+  var scriptContent = "\n    // Parse and run the method with its arguments.\n    (".concat(stringifiedMethod, ")(...").concat(stringifiedArgs, ");\n\n    // Remove the script element to cover our tracks.\n    document.currentScript.parentElement\n      .removeChild(document.currentScript);\n  "); // Create a script tag and inject it into the document.
+
+  var scriptElement = document.createElement('script');
+  scriptElement.innerHTML = scriptContent;
+  document.documentElement.prepend(scriptElement);
+}; // Break out of the sandbox and run `overwriteLanguage()` in the page context.
+
+
+console.log('hello from contentScript');
+console.log('Goodbye from contentScript');
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=contentScript.js.map
