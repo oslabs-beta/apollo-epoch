58107242c41bb27d1775af228e0158f963b51e21
Ma-Christopher <christopher.john.ma@gmail.com>
2020-08-10 19:32:55 -0700

    Fixes typos in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './containers/App';
 
-ReactDom.render(<App />, document.getElementById('root'));
-
-console.log('hello from index.tsx');
+ReactDOM.render(<App />, document.getElementById('root'));
