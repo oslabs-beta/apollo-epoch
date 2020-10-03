2946d9a780696865afd8ca295bc29f140ed810ec
Matt Severyn <mseveryn@gmail.com>
2020-10-03 11:04:23 -0700

    Added Message Path from button to window for timeTravel Initiation

--- chromeExtension/contentScript.js
+++ chromeExtension/contentScript.js
@@ -78,6 +78,16 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     const { queryCount, mutationCount } = counts.getCounts();
     injectAndRunInDom(sendMessageWithCache, queryCount, mutationCount, false, true); // fire manually to bypass counts check
   }
+
+  // Create State Snapshot when Q/M/Manual Fetch is stored in Redux
+  if (message.type === epoch.createSnapshot) {
+    window.postMessage({ type: epoch.createSnapshot, payload: message.payload }, '*');
+  }
+
+  // Initiate Apollo State Change (payload should be Apollo Action Id)
+  if (message.type === epoch.epochShift) {
+    window.postMessage({ type: epoch.epochShift, payload: message.payload }, '*');
+  }
 });
 
 /*
--- src/components/HistoryViewQuery.jsx
+++ src/components/HistoryViewQuery.jsx
@@ -2,13 +2,21 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { initiateEpochShift } from '../store/entities/apollo';
 
 const epochTheme = createMuiTheme({ palette: { primary: { main: '#20909f' } } });
 
 const HistoryViewQuery = ({ timelineObj, onClick, active }) => {
+  const dispatch = useDispatch();
   const { type, id, name } = timelineObj;
   const typeAbbrevs = { Query: 'Q', Mutation: 'M', 'Manual Fetch': 'MF' };
+
+  const epochShift = (apolloActionId) => {
+    dispatch(initiateEpochShift({ apolloActionId }));
+  };
+
   return (
     <div
       className={`query-card ${active && 'active-query'}`}
@@ -24,7 +32,12 @@ const HistoryViewQuery = ({ timelineObj, onClick, active }) => {
               <div className="time-stamp">
                 <p>No Time</p>
               </div>
-              <div className="jump-button">
+              <div
+                className="jump-button"
+                onClick={() => {
+                  epochShift(id);
+                }}
+              >
                 <p>Jump</p>
               </div>
             </div>
