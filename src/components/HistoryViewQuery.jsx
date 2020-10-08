fe99a67e57c2a5b896de8e903d3bf9a14c5c4c4e
Jonathan Ramirez <thecodeswitcher@gmail.com>
2020-10-07 20:18:39 -0500

    Making jump button conditionally render on non-cache queries

--- src/components/HistoryViewQuery.jsx
+++ src/components/HistoryViewQuery.jsx
@@ -26,24 +26,28 @@ const HistoryViewQuery = ({ timelineObj, onClick, active }) => {
     >
       <div className="query-card-text">
         <div>{`${id}: ${name}`}</div>
-        {isNetwork && <img src={networkIcon} alt="network" className="network-icon" />}
-        <ThemeProvider theme={epochTheme}>
-          <div className="button-switcher-container">
-            <div className="button-switcher">
-              <div className="time-stamp">
-                <p>No Time</p>
+        <div className="network-icon-and-jump">
+          {isNetwork && <img src={networkIcon} alt="network" className="network-icon" />}
+          <ThemeProvider theme={epochTheme}>
+            {id[0] !== 'F' && (
+              <div className="button-switcher-container">
+                <div className="button-switcher">
+                  <div className="time-stamp">
+                    <p>No Time</p>
+                  </div>
+                  <div
+                    className="jump-button"
+                    onClick={() => {
+                      epochShift(id);
+                    }}
+                  >
+                    <p>Jump</p>
+                  </div>
+                </div>
               </div>
-              <div
-                className="jump-button"
-                onClick={() => {
-                  epochShift(id);
-                }}
-              >
-                <p>Jump</p>
-              </div>
-            </div>
-          </div>
-        </ThemeProvider>
+            )}
+          </ThemeProvider>
+        </div>
       </div>
     </div>
   );
